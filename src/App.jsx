ce8706c80e1c45c00723361a022b0b21d34aca06
ABISHAGHIMIRE <ag736s@missouristate.edu>
2024-06-13 13:26:55 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+import Cards from './components/cards.jsx'
+
+function App() {
+  const[count, setCount] = useState(0);
+  const[multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount (count + multiplier);
+
+  const buyDoubleStuffed = () => {
+    if(count >=10){
+      setMultiplier(multiplier * 2);
+      setCount (count -10);
+    }
+  }
+
+  const buyPartyPack = () =>{
+    if (count >=100){
+      setMultiplier (multiplier * 5);
+      setCount (count -100);
+    }
+  }
+
+  const buyFullFeast = () =>{
+    if (count >=1000){
+      setMultiplier (multiplier * 10);
+      setCount (count -1000);
+    }
+  }
+  
+  return (
+  <div className="App">
+
+    <div className='header'>
+      <h1>Samosa Selector</h1>
+      <h2> Count: {count}</h2>
+      <img className= "samosa" src = "image.png" onClick={updateCount}/>
+    </div>
+
+    <div className='container'> 
+      <div className='upgrade'>
+        <h3> Double stuffed</h3>
+        <p> 2X per click</p>
+        <button onClick= {buyDoubleStuffed}> 10 samosas</button>
+      </div>
+
+      <div className='upgrade'>
+        <h3> Party Pack</h3>
+        <p> 5X per click</p>
+        <button onClick= {buyPartyPack}> 100 samosas</button>
+      </div>
+
+      <div className='upgrade'>
+        <h3> Full feast</h3>
+        <p> 10X per click</p>
+        <button onClick= {buyFullFeast}> 1000 samosas</button>
+      </div>
+    </div>
+    
+    
+  </div>
+  
+ )
+}
+
+export default App
---
+++ src/components/cards.jsx
@@ -0,0 +1,13 @@
+
+const Cards = () =>
+    {
+        return (
+            <div className="cards">
+                 <h3>Double Stuffed</h3>
+            </div>
+           
+
+        )
+    }
+
+export default Cards
